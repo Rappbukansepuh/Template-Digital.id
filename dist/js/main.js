65c4cb5b03514f98f48f436a8e55a1faf98f7bcc
rafaghzli <rafaghazali2202@gmail.com>
2026-06-19 16:25:16 +0700

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,43 +1,43 @@
 var typed = new Typed('#element', {
-      strings: ['Rafa Ghazali', 'Web Developer', 'Ui/Ux Designer'],
-      typeSpeed: 50,
-    });
+    strings: ['Rafa Ghazali', 'Web Developer', 'Ui/Ux Designer'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
 
-
-const hamburger = document.querySelector
-(".ri-menu-3-line");
-
+const hamburger = document.querySelector(".ri-menu-3-line");
 const menu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
-    menu.classList.toggle("menu-active")
+    menu.classList.toggle("menu-active");
+    hamburger.classList.toggle("ri-close-large-line");
 });
 
 window.onscroll = () => {
     menu.classList.remove("menu-active");
-}
+    hamburger.classList.remove("ri-close-large-line");
+};
 
-const btnFilter = document.querySelectorAll
-(".produk-box ul li")
+const btnFilter = document.querySelectorAll(".produk-box ul li");
 const itemImg = document.querySelectorAll(".produk-list img");
 
 btnFilter.forEach((data) => {
-
     data.onclick = () => {
-        btnFilter.forEach((data) => {
-            data.className = "";
+        btnFilter.forEach((btn) => {
+            btn.className = "";
         });
 
-      data.className = "active";
+        data.className = "active";
 
-      // filter active 
-      const btnText = data.textContent;
+        const btnText = data.textContent.trim().toLowerCase();
+        
         itemImg.forEach((img) => {
             img.style.display = "none";
-            if (img.getAttribute("data-filter") == 
-            btnText.toLowerCase() || btnText == "All Produk") {
-            img.style.display = "block";
-     }
-    });
-  };
+            const imgFilter = img.getAttribute("data-filter") ? img.getAttribute("data-filter").toLowerCase() : "";
+            
+            if (imgFilter === btnText || btnText === "all produk") {
+                img.style.display = "block";
+            }
+        });
+    };
 });
